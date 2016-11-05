20754ddb84cb9f94e742fd809b83896da6306485
Dan Buda <danbuda@gmail.com>
2016-11-05 15:36:48 -0500

    basic structure created

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,18 +1,17 @@
-// import the stylesheet. this is necessary so that webpack will compile all the sass into css and then build it into our style.css file
 import './../styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Links from './app';
 
-// import a module from another file.
-import tiy from './app.js';
+const nav = (
+	<div>
+		<span>My Logo</span>
+		<Links breakpoint="480px">
+			<a href="#">Home</a>
+			<a href="#">Blog</a>
+			<a href="#">Projects</a>
+		</Links>
+	</div>
+);
 
-// Looks like the imported module was a function, because here we're executing it!
-tiy();
-
-const TestComponent = React.createClass({
-	render: function() {
-		return <h1>Test~</h1>;
-	}
-});
-
-ReactDOM.render(<TestComponent />, document.querySelector('main'));
+ReactDOM.render(nav, document.getElementById('app'));
